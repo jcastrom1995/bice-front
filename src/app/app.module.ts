55db5e495635040d918363f4f682500b6c5d272e
Javier Castro <jcastro@geosolve.cl>
2020-12-25 20:01:13 -0300

    Create Aside component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsideComponent } from './aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
