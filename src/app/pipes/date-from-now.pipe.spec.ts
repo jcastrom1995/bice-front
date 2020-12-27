a7e442a78533572f3c0b341737ce18983f13318c
Javier Castro <jcastro@geosolve.cl>
2020-12-27 20:00:40 -0300

    Change unit test name

--- src/app/pipes/date-from-now.pipe.spec.ts
+++ src/app/pipes/date-from-now.pipe.spec.ts
@@ -1,14 +1,14 @@
 import { DateFromNowPipe } from './date-from-now.pipe';
 
-describe('TestAbcPipe', () => {
+describe('Test date pipe', () => {
   const pipe = new DateFromNowPipe();
   it('transform date unix to format DD-MM-YYYY', () => {
     expect(pipe.transform(1580428800)).toBe('30-01-2020');
   });
-  it('transform not unix date', () => {
+  it('transform random unix date', () => {
     expect(pipe.transform(124123)).toBe('02-01-1970');
   });
-  it('transform string to date', () => {
+  it('transform invalid date', () => {
     expect(pipe.transform('ascnasja')).toBe('Fecha inválida');
   });
 });
